2f5f2f5624f8f2d34643c80e2141b3ab66fa2edb
Jae Shin <J@Jaes-MacBook-Air.local>
2021-05-25 20:10:04 -0500

    Created view pages for api/first_product_path

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,5 @@
 class Api::ProductsController < ApplicationController
+  def first_product_action
+    render "first_product.json.jb"
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create([{ name: "Skateboard"}, {price: 100}, {description: "All the cool kids do it."}])
+product.save
+
+product = Product.create([{ name: "Bicycle"}, {price: 500}, {description: "Ride around town like a boss."}])
+product.save
+
+product = Product.create([{ name: "Computer"}, {price: 1000}, {description: "Play all the games you can imagine."}])
+product.save
+
+product = Product.create([{ name: "Dumbbells"}, {price: 25}, {description: "Get swole or die."}])
+product.save
+
+product = Product.create([{ name: "Headphones"}, {price: 300}, {description: "Probably the best audio you'll ever hear."}])
+product.save
+
+product = Product.create([{ name: "Camera"}, {price: 1500}, {description: "Capture all the precious moments in life."}])
+product.save
+
+product = Product.create([{ name: "Rolex"}, {price: 10000}, {description: "You wish you can afford this."}])
+product.save
+
+  
+
+
+
+    
