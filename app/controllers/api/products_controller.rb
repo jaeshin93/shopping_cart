30e0a37924efc1ecdc20deb7780115cd4af6deb1
Jae Shin <jaeshin93@gmail.com>
2021-06-08 19:46:13 -0500

    Added all RESTful routes/controllers/views

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,9 +4,36 @@ class Api::ProductsController < ApplicationController
     render "index.json.jb"
   end
 
-  def show
-    product_id = params[:id].to_i
-    @product = Product.find_by(id: product_id)
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    @product.save
     render "show.json.jb"
   end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: { message: "Product destroyed successfully!" }
+  end
+
 end
