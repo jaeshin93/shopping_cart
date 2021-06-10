27559b240a0fbb2d4fa456af5ed64102496cf389
Jae Shin <jaeshin93@gmail.com>
2021-06-09 21:32:47 -0500

    Added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,21 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    tax + price
+  end
 end
