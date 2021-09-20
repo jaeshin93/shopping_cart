a6b5b6aef8a9182b15ceb3146183929f207f5b05
jaeshin93 <jaeshin93@gmail.com>
2021-09-19 19:23:13 -0500

    New carted_product controller made
    routed new index and show requests for carted_products. Authorization for current_user

--- app/controllers/api/carted_products_controller.rb
+++ app/controllers/api/carted_products_controller.rb
@@ -1,2 +1,22 @@
 class Api::CartedProductsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    # @carted_products = CartedProduct.where(user_id: current_user.id, status: "carted")
+    # either line of code works but prefer bottom line since it utilizes the associations made
+    @carted_products = current_user.carted_products.where(status: "carted")
+    render "index.json.jb"
+  end
+
+  def create
+    @carted_product = CartedProduct.new(
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      user_id: current_user.id,
+      status: "carted",
+    )
+    @carted_product.save
+    render "show.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
     get "/orders" => "orders#index"
     post "/orders" => "orders#create"
     get "/orders/:id" => "orders#show"
+
+    get "/carted_products" => "carted_products#index"
+    post "/carted_products" => "carted_products#create"
   end
 end
  
